7a8ca749834d9794192b608a19bbcecedc36df87
Sarvesh Yadav <sarvesh.y305@gmail.com>
2017-09-20 14:18:46 +0530

    Create CheckGC.java

---
+++ OOPS/src/com/CheckGC.java
@@ -0,0 +1,21 @@
+public class CheckGC {
+	public static void main(String[] args) {
+		Runtime rt = Runtime.getRuntime();
+		System.out.println("total JVM memory :" + rt.totalMemory());
+		Date d = null;
+		for (int i = 0; i < 10000; i++) {
+			for (int j = 0; j < 10000; j++) {
+				for (int k = 0; j < 10000; j++) {
+					for (int m = 0; j < 10000; j++) {
+						new Date();
+					}
+				}
+			}
+			d = new Date();
+			d = null;
+		}
+		System.out.println("after memory : "+rt.freeMemory());
+		rt.gc();
+		System.out.println("after gc memory : "+rt.freeMemory());
+	}
+}
